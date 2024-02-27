b41d355d09054921e0cce279c6b0c50845073252
Talha Hasan18 <mtbanwar18@gmail.com>
2024-02-27 14:59:12 +0500

    second assignment

---
+++ main.js
@@ -0,0 +1,3 @@
+"use strict";
+let personName = "Talha";
+console.log(`hello, ${personName}! would you like to learn some python today?`);
---
+++ main.ts
@@ -0,0 +1,2 @@
+let personName: string = "Talha";
+console.log(`hello, ${personName}! would you like to learn some python today?`);
